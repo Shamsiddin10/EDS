4e615c879ff7f3ecadd5b93888d952acdd46a7ed
Shamsiddin10 <asliddinibrohimjonov56@gmail.com>
2026-05-04 15:15:30 +0500

    fix render port binding error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,10 +32,8 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
